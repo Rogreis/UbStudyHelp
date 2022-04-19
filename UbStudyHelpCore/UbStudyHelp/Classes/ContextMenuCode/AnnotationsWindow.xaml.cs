3fd62c047163344b419cb4fdacf5d89a24d42f10
Rogério Reis da Silva <rogreis@gmail.com>
2022-04-19 16:07:23 +0100

    Annotations window finished

--- UbStudyHelpCore/UbStudyHelp/Classes/ContextMenuCode/AnnotationsWindow.xaml.cs
+++ UbStudyHelpCore/UbStudyHelp/Classes/ContextMenuCode/AnnotationsWindow.xaml.cs
@@ -32,6 +32,9 @@ namespace UbStudyHelp.Classes
             this.Unloaded += AnnotationsWindow_Unloaded;
             Annotations.Entry = entry;
 
+            EventsControl.FontChanged += EventsControl_FontChanged;
+            EventsControl.AppearanceChanged += EventsControl_AppearanceChanged;
+
             this.Title = $"Paragraph Annotations for {entry.ParagraphID}";
 
             Entry= entry;
@@ -51,8 +54,35 @@ namespace UbStudyHelp.Classes
 
             Annotations.StartAnnotations(FlowDocument);
 
-            lblCheck.Content= entry.Description;
+            //RichTextBoxNote.AppendText(entry.Description);
+            SetFontSize();
+            SetAppearence();
+        }
 
+
+        private void SetFontSize()
+        {
+            App.Appearance.SetFontSize(RichTextBoxNote);
+            App.Appearance.SetFontSize(FlowDocument);
+            //App.Appearance.SetFontSize(ToolBarAnnotations);
+        }
+
+        private void SetAppearence()
+        {
+            App.Appearance.SetThemeInfo(RichTextBoxNote);
+            App.Appearance.SetThemeInfo(FlowDocument);
+            //App.Appearance.SetThemeInfo(ToolBarAnnotations);
+        }
+
+
+        private void EventsControl_AppearanceChanged(ControlsAppearance appearance)
+        {
+            SetAppearence();
+        }
+
+        private void EventsControl_FontChanged(Classes.ControlsAppearance appearance)
+        {
+            SetFontSize();
         }
 
 
@@ -62,6 +92,9 @@ namespace UbStudyHelp.Classes
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Copy", ApplicationCommands.Copy));
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Select all", ApplicationCommands.SelectAll));
             FlowDocument.ContextMenu.Items.Add(new Separator());
+            FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Print Preview", ApplicationCommands.PrintPreview));
+            FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Print", ApplicationCommands.Print));
+            FlowDocument.ContextMenu.Items.Add(new Separator());
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Add Highlight", AnnotationService.CreateHighlightCommand));
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Add Text Note", AnnotationService.CreateTextStickyNoteCommand));
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Add Ink Note", AnnotationService.CreateInkStickyNoteCommand));
@@ -69,6 +102,27 @@ namespace UbStudyHelp.Classes
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Remove Highlights", AnnotationService.ClearHighlightsCommand));
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Remove Notes", AnnotationService.DeleteStickyNotesCommand));
             FlowDocument.ContextMenu.Items.Add(CreateMenuItem("Remove Highlights & Notes", AnnotationService.DeleteAnnotationsCommand));
+
+
+            RichTextBoxNote.ContextMenu = new ContextMenu();
+            RichTextBoxNote.ContextMenu.Items.Add(CreateMenuItem("Copy", ApplicationCommands.Copy));
+            RichTextBoxNote.ContextMenu.Items.Add(CreateMenuItem("Cut", ApplicationCommands.Cut));
+            RichTextBoxNote.ContextMenu.Items.Add(CreateMenuItem("Paste", ApplicationCommands.Paste));
+            RichTextBoxNote.ContextMenu.Items.Add(new Separator());
+            RichTextBoxNote.ContextMenu.Items.Add(CreateMenuItem("Select all", ApplicationCommands.SelectAll));
+
+
+            //ToolBarAnnotations.Items.Add(new Separator());
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Copy", ApplicationCommands.Copy));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Select All", ApplicationCommands.SelectAll));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Print", ApplicationCommands.Print));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Find", ApplicationCommands.Find));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Open", ApplicationCommands.Open));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Save", ApplicationCommands.Save));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Save As", ApplicationCommands.SaveAs));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Close", ApplicationCommands.Close));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Undo", ApplicationCommands.Undo));
+            //ToolBarAnnotations.Items.Add(CreateMenuItem("Redo", ApplicationCommands.Redo));
         }
 
         private void AnnotationsWindow_Unloaded(object sender, RoutedEventArgs e)
